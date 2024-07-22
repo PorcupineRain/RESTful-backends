ff0c5c04aacadb316a7624e7ea7b626599c8ad7e
rayne <rayne.jurchen@gmail.com>
2024-07-22 20:08:14 +0200

    refactor code

--- fetch-api-and-http/01-quote-app/script.js
+++ fetch-api-and-http/01-quote-app/script.js
@@ -1,16 +1,15 @@
 const button = document.querySelector('button');
-const quoteTextElement = document.querySelector('#quote-text');
-const quoteAuthorElement = document.querySelector('#author');
+const quoteText = document.querySelector('#quote-text');
+const quoteAuthor = document.querySelector('#author');
 
 
 button.addEventListener("click", function(){
-fetch("https://dummy-apis.netlify.app/api/quote")
-.then ((response) => {
-    if (response.ok){return response.json()};
-})
-.then((data)=> {
-    quoteTextElement.innerText = '"' + data.quote + '"';
-    quoteAuthorElement.innerText = data.author;
-})
-
+    fetch("https://dummy-apis.netlify.app/api/quote")
+    .then ((response) => {
+        if (response.ok){return response.json()};
+    })
+    .then((data)=> {
+     quoteText.innerText = '"' + data.quote + '"';
+     quoteAuthor.innerText = data.author;
+    })
 })
