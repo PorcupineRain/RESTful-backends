1404ca1168a5b2759dbd328c27cb41955ebcbb91
rayne <rayne.jurchen@gmail.com>
2024-07-22 21:30:18 +0200

    add working random color button with api

---
+++ fetch-api-and-http/02-color-mixer-with-api/script.js
@@ -0,0 +1,68 @@
+const body = document.querySelector("body");
+const p = document.querySelector("p");
+const slider1 = document.querySelector("#red");
+const slider2 = document.querySelector("#green");
+const slider3 = document.querySelector("#blue");
+const button = document.querySelector("button")
+
+
+slider1.addEventListener("input", function () {
+  changeBackground();
+});
+slider2.addEventListener("input", function () {
+  changeBackground();
+});
+slider3.addEventListener("input", function () {
+  changeBackground();
+});
+
+function changeBackground() {
+  let red = slider1.value;
+  let green = slider2.value;
+  let blue = slider3.value;
+  let hexR = parseInt(red).toString(16);
+  let hexG = parseInt(green).toString(16);
+  let hexB = parseInt(blue).toString(16);
+  if (hexR.length === 1) {
+    hexR = "0" + hexR;
+  }
+  if (hexG.length === 1) {
+    hexG = "0" + hexG;
+  }
+  if (hexB.length === 1) {
+    hexB = "0" + hexB;
+  }
+  p.innerText = "#" + hexR + hexG + hexB;
+  body.style.background = "rgba(" + red + "," + green + "," + blue + ")";
+}
+
+changeBackground();
+
+button.addEventListener("click", function(){
+    fetch("https://dummy-apis.netlify.app/api/color")
+    .then((response) =>{
+        if(response.ok){
+            return response.json();
+        }
+    })
+    .then((data) => {
+        let red = data.rgb.r;
+        let green = data.rgb.g;
+        let blue = data.rgb.b;
+        let hexR = parseInt(red).toString(16);
+  let hexG = parseInt(green).toString(16);
+  let hexB = parseInt(blue).toString(16);
+  if (hexR.length === 1) {
+    hexR = "0" + hexR;
+  }
+  if (hexG.length === 1) {
+    hexG = "0" + hexG;
+  }
+  if (hexB.length === 1) {
+    hexB = "0" + hexB;
+  }
+  p.innerText = "#" + hexR + hexG + hexB;
+  body.style.background = "rgba(" + red + "," + green + "," + blue + ")";
+   
+    })
+})
