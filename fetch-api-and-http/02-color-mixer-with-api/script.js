89e124b6f1aa595247463a420fed7e4276307374
rayne <rayne.jurchen@gmail.com>
2024-07-22 21:39:52 +0200

    refactoring

--- fetch-api-and-http/02-color-mixer-with-api/script.js
+++ fetch-api-and-http/02-color-mixer-with-api/script.js
@@ -3,8 +3,27 @@ const p = document.querySelector("p");
 const slider1 = document.querySelector("#red");
 const slider2 = document.querySelector("#green");
 const slider3 = document.querySelector("#blue");
-const button = document.querySelector("button")
+const button = document.querySelector("button");
 
+function changeBackground() {
+    let red = slider1.value;
+    let green = slider2.value;
+    let blue = slider3.value;
+    let hexR = parseInt(red).toString(16);
+    let hexG = parseInt(green).toString(16);
+    let hexB = parseInt(blue).toString(16);
+    if (hexR.length === 1) {
+      hexR = "0" + hexR;
+    }
+    if (hexG.length === 1) {
+      hexG = "0" + hexG;
+    }
+    if (hexB.length === 1) {
+      hexB = "0" + hexB;
+    }
+    p.innerText = "#" + hexR + hexG + hexB;
+    body.style.background = "rgba(" + red + "," + green + "," + blue + ")";
+  }
 
 slider1.addEventListener("input", function () {
   changeBackground();
@@ -15,28 +34,6 @@ slider2.addEventListener("input", function () {
 slider3.addEventListener("input", function () {
   changeBackground();
 });
-
-function changeBackground() {
-  let red = slider1.value;
-  let green = slider2.value;
-  let blue = slider3.value;
-  let hexR = parseInt(red).toString(16);
-  let hexG = parseInt(green).toString(16);
-  let hexB = parseInt(blue).toString(16);
-  if (hexR.length === 1) {
-    hexR = "0" + hexR;
-  }
-  if (hexG.length === 1) {
-    hexG = "0" + hexG;
-  }
-  if (hexB.length === 1) {
-    hexB = "0" + hexB;
-  }
-  p.innerText = "#" + hexR + hexG + hexB;
-  body.style.background = "rgba(" + red + "," + green + "," + blue + ")";
-}
-
-changeBackground();
 
 button.addEventListener("click", function(){
     fetch("https://dummy-apis.netlify.app/api/color")
